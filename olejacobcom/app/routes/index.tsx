53bc0bb799cb673eab2e07464c464b8dcaa8552d
Ole Jacob Brunstad <38566764+olejbl@users.noreply.github.com>
2022-05-25 01:07:44 +0200

    init

---
+++ olejacobcom/app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{width: "100vw", height: "100vw", backgroundImage:"url(R1-04081-0036.JPG)", backgroundSize: "cover", overflow: "hidden" ,fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <div style={{zIndex:2}}><h1>Ole Jacob</h1></div>
+    </div>
+  );
+}

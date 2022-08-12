b80a539b59ed1152c37051d1bb9355e0661396ca
olejbl <38566764+olejbl@users.noreply.github.com>
2022-08-12 16:22:39 +0200

    testing resemblejs

---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+
+const compareImages = require("resemblejs/compareImages");
+const fs = require("mz/fs");
+
+async function getDiff() {
+  const options = {
+    output: {
+      errorColor: {
+        red: 255,
+        green: 0,
+        blue: 255,
+      },
+      errorType: "movement",
+      transparency: 0.3,
+      largeImageThreshold: 1200,
+      useCrossOrigin: false,
+      outputDiff: true,
+    },
+    scaleToSameSize: true,
+    ignore: "antialiasing",
+  };
+
+  const data = await compareImages(
+    await fs.readFile("./People.jpg"),
+    await fs.readFile("./People2.jpg"),
+    options
+  );
+
+  await fs.writeFile("./output.png", data.getBuffer());
+}
+
+getDiff();
+
+export default function Home() {
+  return (
+    <div>
+      Hello World.
+      <Link href="/about">About</Link>
+    </div>
+  );
+}
